1194873dbecc165969ae22d73decb383d85b0df7
stanislasbdx <perso@stan1712.fr>
2018-04-01 04:42:55 +0200

    Optimization for the new parameter

--- src/fr/stan1712/srp/srp.java
+++ src/fr/stan1712/srp/srp.java
@@ -30,6 +30,9 @@ public void onEnable(){
 		if(getConfig().getBoolean("TownSystem") == true) {
 			System.out.println("[SeriousRP] Town module activated");
 		}
+		if(getConfig().getBoolean("CustomRecipes") == true) {
+			System.out.println("[SeriousRP] Custom recipes module activated");
+		}
 		System.out.println("[SeriousRP] All classes are loaded !");
 		System.out.println("[SeriousRP] #------------#");
 		
@@ -41,52 +44,73 @@ public void onEnable(){
 		pm.registerEvents(new spawnitems(), this);
 		pm.registerEvents(new phone(this), this);
 		
-		getConfig().options().copyDefaults(false);
+		getConfig().options().copyDefaults(true);
 	    saveDefaultConfig();
 		
-		ShapedRecipe saddle =  new ShapedRecipe(new ItemStack(Material.SADDLE, 1));
-		saddle.shape(new String[] {" C ","CFC","I I"});
-		saddle.setIngredient('C', Material.LEATHER);
-		saddle.setIngredient('I', Material.STRING);
-		saddle.setIngredient('F', Material.IRON_INGOT);
-		getServer().addRecipe(saddle);
-		
-		ShapedRecipe nametag = new ShapedRecipe(new ItemStack(Material.NAME_TAG));
-		nametag.shape(new String[] {"  I"," B ","P  "});
-		nametag.setIngredient('P', Material.PAPER);
-		nametag.setIngredient('I', Material.STRING);
-		nametag.setIngredient('B', Material.BOOK_AND_QUILL);
-		getServer().addRecipe(nametag);
-		
-		ShapedRecipe horseiron = new ShapedRecipe(new ItemStack(Material.IRON_BARDING));
-		horseiron.shape(new String[] {"   ","IBI","II "});
-		horseiron.setIngredient('I', Material.IRON_INGOT);
-		horseiron.setIngredient('B', Material.IRON_BLOCK);
-		getServer().addRecipe(horseiron);
-		
-		ShapedRecipe horsegold = new ShapedRecipe(new ItemStack(Material.GOLD_BARDING));
-		horsegold.shape(new String[] {"   ","IBI","II "});
-		horsegold.setIngredient('I', Material.GOLD_INGOT);
-		horsegold.setIngredient('B', Material.GOLD_BLOCK);
-		getServer().addRecipe(horsegold);
-		
-		ShapedRecipe horsediamond = new ShapedRecipe(new ItemStack(Material.DIAMOND_BARDING));
-		horsediamond.shape(new String[] {"   ","IBI","II "});
-		horsediamond.setIngredient('I', Material.DIAMOND);
-		horsediamond.setIngredient('B', Material.DIAMOND_BLOCK);
-		getServer().addRecipe(horsediamond);
-		
-		ShapedRecipe steak = new ShapedRecipe(new ItemStack(Material.RAW_BEEF));
-		steak.shape(new String[] {"RRR","RBR","RRR"});
-		steak.setIngredient('B', Material.BOWL);
-		steak.setIngredient('R', Material.ROTTEN_FLESH);
-		getServer().addRecipe(steak);
+	    if(getConfig().getBoolean("CustomRecipes") == true) {
+			ShapedRecipe saddle =  new ShapedRecipe(new ItemStack(Material.SADDLE, 1));
+			saddle.shape(new String[] {" C ","CFC","I I"});
+			saddle.setIngredient('C', Material.LEATHER);
+			saddle.setIngredient('I', Material.STRING);
+			saddle.setIngredient('F', Material.IRON_INGOT);
+			getServer().addRecipe(saddle);
+			
+			ShapedRecipe nametag = new ShapedRecipe(new ItemStack(Material.NAME_TAG));
+			nametag.shape(new String[] {"  I"," B ","P  "});
+			nametag.setIngredient('P', Material.PAPER);
+			nametag.setIngredient('I', Material.STRING);
+			nametag.setIngredient('B', Material.BOOK_AND_QUILL);
+			getServer().addRecipe(nametag);
+			
+			ShapedRecipe horseiron = new ShapedRecipe(new ItemStack(Material.IRON_BARDING));
+			horseiron.shape(new String[] {"   ","IBI","II "});
+			horseiron.setIngredient('I', Material.IRON_INGOT);
+			horseiron.setIngredient('B', Material.IRON_BLOCK);
+			getServer().addRecipe(horseiron);
+			
+			ShapedRecipe horsegold = new ShapedRecipe(new ItemStack(Material.GOLD_BARDING));
+			horsegold.shape(new String[] {"   ","IBI","II "});
+			horsegold.setIngredient('I', Material.GOLD_INGOT);
+			horsegold.setIngredient('B', Material.GOLD_BLOCK);
+			getServer().addRecipe(horsegold);
+			
+			ShapedRecipe horsediamond = new ShapedRecipe(new ItemStack(Material.DIAMOND_BARDING));
+			horsediamond.shape(new String[] {"   ","IBI","II "});
+			horsediamond.setIngredient('I', Material.DIAMOND);
+			horsediamond.setIngredient('B', Material.DIAMOND_BLOCK);
+			getServer().addRecipe(horsediamond);
+			
+			ShapedRecipe steak = new ShapedRecipe(new ItemStack(Material.RAW_BEEF));
+			steak.shape(new String[] {"RRR","RBR","RRR"});
+			steak.setIngredient('B', Material.BOWL);
+			steak.setIngredient('R', Material.ROTTEN_FLESH);
+			getServer().addRecipe(steak);
+			
+			ShapedRecipe gunpowder = new ShapedRecipe(new ItemStack(Material.SULPHUR));
+			gunpowder.shape(new String[] {"CRS","   ","   "});
+			gunpowder.setIngredient('C', Material.COAL);
+			gunpowder.setIngredient('R', Material.REDSTONE);
+			gunpowder.setIngredient('S', Material.SUGAR);
+			getServer().addRecipe(gunpowder);
+	    }
 	}
   
 public void onDisable(){
 		System.out.println("[SeriousRP] #------------#");
 		System.out.println("[SeriousRP] SeriousRP "+getConfig().getString("Version").replace("&", "§"));
 		System.out.println("[SeriousRP] Shutdown sequence launched");
+		if(getConfig().getBoolean("RPDeath") == true) {
+			System.out.println("[SeriousRP] RealDeath module deactivated");
+		}
+		if(getConfig().getBoolean("Medics") == true) {
+			System.out.println("[SeriousRP] Medics module deactivated");
+		}
+		if(getConfig().getBoolean("TownSystem") == true) {
+			System.out.println("[SeriousRP] Town module deactivated");
+		}
+		if(getConfig().getBoolean("CustomRecipes") == true) {
+			System.out.println("[SeriousRP] Custom recipes module deactivated");
+		}
 		System.out.println("[SeriousRP] Classes disabled");
 		System.out.println("[SeriousRP] #------------#");
 	}
