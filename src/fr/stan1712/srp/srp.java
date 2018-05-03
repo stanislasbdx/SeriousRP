b8e44edf7185d3e30549529e97f547c93d66f327
stanislasbdx <perso@stan1712.fr>
2018-05-04 00:37:51 +0200

    New Config Manager

--- src/fr/stan1712/srp/srp.java
+++ src/fr/stan1712/srp/srp.java
@@ -2,27 +2,14 @@ package fr.stan1712.srp;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.logging.Logger;
-
-import org.bukkit.Bukkit;
-import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.configuration.file.FileConfiguration;
-import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.event.Listener;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.ShapedRecipe;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 import fr.stan1712.srp.commands;
-import net.milkbowl.vault.chat.Chat;
-import net.milkbowl.vault.economy.Economy;
-import net.milkbowl.vault.economy.EconomyResponse;
-import net.milkbowl.vault.permission.Permission;
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Player;
-import org.bukkit.plugin.RegisteredServiceProvider;
 
 
 public class srp extends JavaPlugin implements Listener{
@@ -59,6 +46,15 @@ public void onEnable(){
 		System.out.println("[SeriousRP] Config file up and runnning !");
 		System.out.println("[SeriousRP] #------------#");
 		
+		File config = new File("plugins/SeriousRP", "config.yml");
+		if (!config.exists()) {
+			try {
+				config.createNewFile();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		
 		PluginManager pm = getServer().getPluginManager();
 		pm.registerEvents(new actions(), this);
 		pm.registerEvents(new commands(this), this);
@@ -68,23 +64,6 @@ public void onEnable(){
 		pm.registerEvents(new phone(this), this);
 		pm.registerEvents(new configupdate(this), this);
 		
-		  File msg = new File("plugins/SeriousRP", "messages.yml");
-	      if (!msg.exists()) {
-	          try {
-	        	  msg.createNewFile();
-	          } catch (IOException e) {
-	              e.printStackTrace();
-	          }
-	      }
-		  File config = new File("plugins/SeriousRP", "config.yml");
-	      if (!config.exists()) {
-	          try {
-	        	  config.createNewFile();
-	          } catch (IOException e) {
-	              e.printStackTrace();
-	          }
-	      }
-	      
 	    saveConfig();
 		
 	    if(getConfig().getBoolean("CustomRecipes") == true) {
@@ -135,97 +114,6 @@ public void onEnable(){
 	    }
 	}
 
-public class ExamplePlugin extends JavaPlugin {
-    
-    private final Logger log = Logger.getLogger("Minecraft");
-    private Economy econ = null;
-    private Permission perms = null;
-    private Chat chat = null;
-
-    @Override
-    public void onDisable() {
-        log.info(String.format("[%s] Disabled Version %s", getDescription().getName(), getDescription().getVersion()));
-    }
-
-    @Override
-    public void onEnable() {
-        if (!setupEconomy() ) {
-            log.severe(String.format("[%s] - Disabled due to no Vault dependency found!", getDescription().getName()));
-            getServer().getPluginManager().disablePlugin(this);
-            return;
-        }
-        setupPermissions();
-        setupChat();
-    }
-    
-    private boolean setupEconomy() {
-        if (getServer().getPluginManager().getPlugin("Vault") == null) {
-            return false;
-        }
-        RegisteredServiceProvider<Economy> rsp = getServer().getServicesManager().getRegistration(Economy.class);
-        if (rsp == null) {
-            return false;
-        }
-        econ = rsp.getProvider();
-        return econ != null;
-    }
-    
-    private boolean setupChat() {
-        RegisteredServiceProvider<Chat> rsp = getServer().getServicesManager().getRegistration(Chat.class);
-        chat = rsp.getProvider();
-        return chat != null;
-    }
-    
-    private boolean setupPermissions() {
-        RegisteredServiceProvider<Permission> rsp = getServer().getServicesManager().getRegistration(Permission.class);
-        perms = rsp.getProvider();
-        return perms != null;
-    }
-    
-    public boolean onCommand(CommandSender sender, Command command, String commandLabel, String[] args) {
-        if(!(sender instanceof Player)) {
-            log.info("Only players are supported for this Example Plugin, but you should not do this!!!");
-            return true;
-        }
-        
-        Player player = (Player) sender;
-        
-        if(command.getLabel().equals("test-economy")) {
-            // Lets give the player 1.05 currency (note that SOME economic plugins require rounding!)
-            sender.sendMessage(String.format("You have %s", econ.format(econ.getBalance(player.getName()))));
-            EconomyResponse r = econ.depositPlayer(player, 1.05);
-            if(r.transactionSuccess()) {
-                sender.sendMessage(String.format("You were given %s and now have %s", econ.format(r.amount), econ.format(r.balance)));
-            } else {
-                sender.sendMessage(String.format("An error occured: %s", r.errorMessage));
-            }
-            return true;
-        } else if(command.getLabel().equals("test-permission")) {
-            // Lets test if user has the node "example.plugin.awesome" to determine if they are awesome or just suck
-            if(perms.has(player, "example.plugin.awesome")) {
-                sender.sendMessage("You are awesome!");
-            } else {
-                sender.sendMessage("You suck!");
-            }
-            return true;
-        } else {
-            return false;
-        }
-    }
-    
-    public Economy getEconomy() {
-        return econ;
-    }
-    
-    public Permission getPermissions() {
-        return perms;
-    }
-    
-    public Chat getChat() {
-        return chat;
-    }
-}
-  
 public void onDisable(){
 		System.out.println("[SeriousRP] #------------#");
 		System.out.println("[SeriousRP] SeriousRP "+getConfig().getString("Version").replace("&", "§"));
