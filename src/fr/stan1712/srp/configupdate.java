2fc28c87d15686113df818f0ddc4df1ca17cf9e2
stanislasbdx <perso@stan1712.fr>
2018-05-04 00:36:26 +0200

    Config Fix and Version
    Added an option for reload the config
    Version updated

--- src/fr/stan1712/srp/configupdate.java
+++ src/fr/stan1712/srp/configupdate.java
@@ -5,64 +5,26 @@ import org.bukkit.event.Listener;
 
 public class configupdate implements Listener {
 	
-	  private FileConfiguration config;
-	  private FileConfiguration messages;
-	  private srp pl;
-	  
-	  public configupdate(srp pl)
+	private srp pl;
+	
+	public configupdate(srp pl)
 	  {
 		  this.pl = pl;
+		  pl.getConfig();
+		  
 		  FileConfiguration config = pl.getConfig();
-		  this.config = pl.getConfig();
+		  pl.getConfig();
 		  
-		  config.set("Version", "Version 3.9.8-2.5b");
-		  config.options().copyDefaults(true);
-	  }
-	  public void messageupdate(srp pl) {
-		  this.pl = pl;
-		  FileConfiguration messages = pl.getConfig();
-		  this.messages = pl.getConfig();
+		  config.set("Version", "Version 3.9.8-3.0g");
+		  config.options().header("SeriousRP | Owner : stan1712 \nTraductors : ErHak_ / legaming04 -> https://github.com/stan1712/SeriousRP/wiki/Translations \nOur Discord : https://discord.gg/DkQSQa7");
 		  
-    	  messages.createSection("HelpMsg");
-		  messages.addDefault("HelpMsg.Dsrtp", "Pouvoir se téléporter aléatoirement");
-		  messages.addDefault("HelpMsg.DTown", "Permet de se téléporter dans la ville");
-		  messages.addDefault("HelpMsg.DTownSet", "Permet de définir le point de spawn de la ville");
-		  messages.addDefault("HelpMsg.DTownWhere", "Permet de savoir ou se trouve la ville");
-		  messages.addDefault("HelpMsg.DPhone", "Affiche le menu du telephone");
-		  messages.addDefault("HelpMsg.DRevive", "Permet de réanimer un joueur");
-		  messages.addDefault("HelpMsg.DMedInfo", "Permet de connaître les statistiques d'un joueur");
-		  messages.addDefault("HelpMsg.DHelp", "Affiche l'aide");
-		  messages.addDefault("HelpMsg.DVersion", "Connaître la version du plugin");
-		  
-		  messages.createSection("Medic");
-		  messages.addDefault("Medic.Revive", "&aVous avez bien reanimé le joueur");
-		  messages.addDefault("Medic.Error", "&cn''est pas en ligne ou vous avez mal orthographie son pseudo !");
-		  messages.addDefault("Medic.NoNeed", "&cLe joueur n''a pas besoin de se faire réanimer");
-		  
-		  messages.createSection("MedInfo");
-		  messages.addDefault("MedInfo.Health", "Vie :");
-		  messages.addDefault("MedInfo.Hearts", "coeurs");
-		  messages.addDefault("MedInfo.Food", "Nourriture :");
-		  messages.addDefault("MedInfo.Coordinates", "est en :");
-		  messages.addDefault("MedInfo.Effects", "Effets");
-		 
-		  messages.createSection("Phone");
-		  messages.addDefault("Phone.LorePhone1", "&9Voici votre téléphone");
-		  messages.addDefault("Phone.LorePhone2", "&9Clic droit pour l''ouvrir");
-		  messages.addDefault("Phone.LorePhone3", "&6Propriétaire :");
-		  messages.addDefault("Phone.Answer", "&aDécrocher");
-		  messages.addDefault("Phone.HangUp", "&cRaccrocher");
-		  messages.addDefault("Phone.Shutdown", "&cEteindre");
-		  messages.addDefault("Phone.You", "&aOui, c''est vous, vous êtes beau non ?");
-		  messages.addDefault("Phone.Music", "&cBientôt...");
-		  messages.addDefault("Phone.GoToTown", "&aDirection : Ville");
-		  messages.addDefault("Phone.YourNumber", "&cBientôt...");
-		  
-		  messages.createSection("Teleports");
-		  messages.addDefault("Teleports.GoToTown", "Vous avez été téléporté dans la ville");
-		  messages.addDefault("Teleports.SetTown", "Vous avez défini le spawn de la ville");
-		  messages.addDefault("Teleports.TownWhere", "Le spawn de la ville se trouve en :");
-		  
-		  messages.options().copyDefaults(true);
+		  if(this.pl.getConfig().getBoolean("ConfigFix") == true) {
+			  config.options().copyDefaults(true); //False, laisse les infos des utilisateurs | True, remplace leurs infos par les miennes
+			  config.options().copyHeader(true);
+			  
+			  config.set("ConfigFix", false);
+			  
+			  System.out.println("[SeriousRP] Config reloaded");
+		  }
 	  }
 }
