d4088635d796adb3d92d6804131c2a040c2a1b4a
stanislasbdx <perso@stan1712.fr>
2018-05-04 19:28:32 +0200

    New config update method

--- src/fr/stan1712/srp/configupdate.java
+++ src/fr/stan1712/srp/configupdate.java
@@ -14,6 +14,15 @@ public class configupdate implements Listener {
 		config.set("Version", "Version 3.9.8-3.0g"); //Changement de version du plugin
 	}
 	
+	private void newaddings() {		  
+		FileConfiguration config = pl.getConfig();
+		pl.getConfig();
+		  
+		config.set("HelpMsg.DRevive", "Réanime et soigne un joueur");
+		config.set("HelpMsg.DMedinfo", "Permet de savoir l'etat de santé d'un joueur");
+		config.set("HelpMsg.DHRRevive", "Vous réanime et vous soigne");
+	}
+	
 	public configupdate(srp pl)
 	  {
 		  this.pl = pl;
@@ -21,6 +30,7 @@ public class configupdate implements Listener {
 		  pl.getConfig();
 		  
 		  versionupdate();
+		  newaddings();
 		  
 		  config.options().header("SeriousRP | Owner : stan1712 \nTraductors : ErHak_ / legaming04 -> https://github.com/stan1712/SeriousRP/wiki/Translations \nOur Discord : https://discord.gg/DkQSQa7");
 		  
@@ -30,7 +40,7 @@ public class configupdate implements Listener {
 			  
 			  config.set("ConfigFix", false);
 			  
-			  System.out.println("[SeriousRP] Config reloaded");
+			  System.out.println("[SeriousRP] Config reloaded !");
 		  }
 	  }
 }
