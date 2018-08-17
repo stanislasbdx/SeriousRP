2af77d1f9d3bd05c7f52e073d03d6751961d33d1
stanislasbdx <perso@stan1712.fr>
2018-08-18 00:20:05 +0200

    config integration

--- src/fr/stan1712/seriousrp/Config.java
+++ src/fr/stan1712/seriousrp/Config.java
@@ -2,37 +2,37 @@ package fr.stan1712.seriousrp;
 
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.event.Listener;
+import org.bukkit.plugin.Plugin;
 
 import net.md_5.bungee.api.ChatColor;
 
 public class Config implements Listener {
-	private Main pl;
+	private Plugin plugin = Main.getPlugin(Main.class);
 
 	private void VersionUpdate() {
-		FileConfiguration config = this.pl.getConfig();
-		this.pl.getConfig();
+		FileConfiguration config = plugin.getConfig();
+		plugin.getConfig();
 		    
 		config.set("Version", "Version 3.9.9");
 	}
 	
-	public Config(Main pl) {
-	    this.pl = pl;
-	    FileConfiguration config = pl.getConfig();
-	    pl.getConfig();
+	public Config() {
+	    FileConfiguration config = plugin.getConfig();
+		plugin.getConfig();
 	    
 	    VersionUpdate();
 	    
 	    config.options().header("SeriousRP | Owner : stan1712 \nTraductors : ErHak_ / legaming04 -> https://github.com/stan1712/SeriousRP/wiki/Translations \nOur Discord : https://discord.gg/DkQSQa7");
-	    if (this.pl.getConfig().getBoolean("ConfigFix"))
+	    if (this.plugin.getConfig().getBoolean("ConfigFix"))
 	    {
 	      config.options().copyDefaults(true);
 	      config.options().copyHeader(true);
 	      
 	      config.set("ConfigFix", Boolean.valueOf(false));
 	      
-	      pl.getServer().getConsoleSender().sendMessage("[SeriousRP] " + ChatColor.GREEN + "config.yml fixed !");
+	      plugin.getServer().getConsoleSender().sendMessage("[SeriousRP] " + ChatColor.GREEN + "config.yml fixed !");
 	    }
 	    
-	    pl.getServer().getConsoleSender().sendMessage("[SeriousRP] " + ChatColor.GREEN + "Config file up and running !");
+		plugin.getServer().getConsoleSender().sendMessage("[SeriousRP] " + ChatColor.GREEN + "Config file up and running !");
 	}
 }
