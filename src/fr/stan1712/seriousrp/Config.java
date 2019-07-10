7fda8c8b6659d2f507261cccc6afd10d129ca4d8
stanislasbdx <perso@stan1712.com>
2019-07-11 01:11:01 +0200

    Version update

--- src/fr/stan1712/seriousrp/Config.java
+++ src/fr/stan1712/seriousrp/Config.java
@@ -9,7 +9,8 @@ import net.md_5.bungee.api.ChatColor;
 public class Config implements Listener {
 	private Plugin plugin = Main.getPlugin(Main.class);
 	
-	String version = "Version 3.9.9-1.3g";
+	String version = "Version 3.9.9-3.0g";
+	String fileVersion = this.plugin.getConfig().getString("Version");
 
 	private void VersionUpdate() {
 		FileConfiguration config = plugin.getConfig();
@@ -25,13 +26,14 @@ public class Config implements Listener {
 	    VersionUpdate();
 	    
 	    config.options().header("SeriousRP | Owner : stan1712 \nTraductors : ErHak_ / legaming04 -> https://github.com/stan1712/SeriousRP/wiki/Translations \nOur Discord : https://discord.gg/DkQSQa7");
+	    
 	    if(this.plugin.getConfig().getBoolean("ConfigFix")) {
 	      config.options().copyDefaults(true);
 	      config.options().copyHeader(true);
 	      
 	      config.set("ConfigFix", Boolean.valueOf(false));
 	      
-	      plugin.getServer().getConsoleSender().sendMessage("[SeriousRP] " + ChatColor.GREEN + "config.yml fixed !");
+	      plugin.getServer().getConsoleSender().sendMessage("[SeriousRP] " + ChatColor.GREEN + "config.yml updated !");
 	    }
 	    
 		plugin.getServer().getConsoleSender().sendMessage("[SeriousRP] " + ChatColor.GREEN + "Config file up and running !");
