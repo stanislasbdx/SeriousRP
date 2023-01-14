6ab7e0dfa8a4bcdea86aa22fa797cf886ee53d78
stanislasbdx <s.castaybert@deal.fr>
2023-01-14 23:58:17 +0100

    Added precisions on startup log

--- src/main/java/fr/stan1712/wetston/seriousrp/Main.java
+++ src/main/java/fr/stan1712/wetston/seriousrp/Main.java
@@ -18,13 +18,14 @@ public final class Main extends JavaPlugin {
 	public static Economy economy = null;
 
 	public void versionCheck() {
+		final String logStep = getName() + " @ versionCheck";
 		final String serverVersion = getServer().getVersion();
-		_log.info("[" + getName() + "] Checking server version : " + serverVersion);
+		_log.info("[" + logStep + "] Checking server version : " + serverVersion);
 
 		final Pattern versionPattern = Pattern.compile("[0-9][.][0-9]+", Pattern.MULTILINE);
 		final Matcher versionMatcher = versionPattern.matcher(serverVersion);
 		if(!versionMatcher.find() || (!serverVersion.contains("Spigot") && !serverVersion.contains("Paper"))) {
-			_log.error("[" + getName() + "] * Version " + serverVersion + "unknown, disabling plugin.");
+			_log.error("[" + logStep + "] * Version " + serverVersion + "unknown, disabling plugin.");
 			pluginManager.disablePlugin(this);
 			return;
 		}
@@ -32,42 +33,43 @@ public final class Main extends JavaPlugin {
 
 		switch (version) {
 			case "1.19", "1.18", "1.17", "1.16", "1.15", "1.14", "1.13" -> {
-				_log.info("[" + getName() + "] Version check !");
-				_log.info("[" + getName() + "] If you got issues, report them on Github");
+				_log.info("[" + logStep + "] Version check !");
+				_log.info("[" + logStep + "] If you got issues, report them on Github");
 			}
 			case "1.12" -> {
-				_log.info("[" + getName() + "] " + version + " may have issues while running !");
-				_log.info("[" + getName() + "] If you got any, report them on Github");
+				_log.info("[" + logStep + "] " + version + " may have issues while running !");
+				_log.info("[" + logStep + "] If you got any, report them on Github");
 			}
 			case "1.11" -> {
-				_log.info("[" + getName() + "] " + version + " is not supported by SeriousRP.");
-				_log.info("[" + getName() + "] If you got any, report them on Github");
+				_log.info("[" + logStep + "] " + version + " is not supported by SeriousRP.");
+				_log.info("[" + logStep + "] If you got any, report them on Github");
 			}
 			default -> {
-				_log.error("[" + getName() + "] * Version is not supported by " + getName() + ", disabling plugin.");
+				_log.error("[" + logStep + "] * Version is not supported by " + getName() + ", disabling plugin.");
 				pluginManager.disablePlugin(this);
 			}
 		}
 	}
 
-	public void dependencyCheck() {
-		_log.info("[" + getName() + "] Dependencies checking...");
+	public void dependenciesCheck() {
+		final String logStep = getName() + " @ dependenciesCheck";
+		_log.info("[" + logStep + "] Dependencies checking...");
 
 		File Tdirect = new File("plugins/Telecom");
 		if (!Tdirect.exists()) {
-			_log.warn("[" + getName() + "] {Telecom} Telecom dependency was not found ! Disabling RPMobile module.");
+			_log.warn("[" + logStep + "] {Telecom} Telecom dependency was not found ! Disabling RPMobile module.");
 			getConfig().set("Core.Modules.RPMobile", Boolean.FALSE);
 		}
 		else {
-			_log.info("[" + getName() + "] {Telecom} Vault hooked !");
+			_log.info("[" + logStep + "] {Telecom} Vault hooked !");
 		}
 
 		if (!setupEconomy() ) {
-			_log.warn("[" + getName() + "] {Vault} Vault dependency was not found ! Disabling Economy modules.");
+			_log.warn("[" + logStep + "] {Vault} Vault dependency was not found ! Disabling Economy modules.");
 			getConfig().set("Core.Modules.Economy", Boolean.FALSE);
 		}
 		else {
-			_log.info("[" + getName() + "] {Vault} Vault hooked !");
+			_log.info("[" + logStep + "] {Vault} Vault hooked !");
 		}
 	}
 
