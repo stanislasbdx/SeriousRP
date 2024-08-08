2f5392edfe0ff3841134252fc6256d5c7be8c88a
stanislasbdx <s.castaybert@deal.fr>
2024-08-08 18:15:40 +0200

    :recycle: Main refacto to get it working and fixed some blocking bugs

--- src/main/java/fr/stan1712/wetston/seriousrp/Main.java
+++ src/main/java/fr/stan1712/wetston/seriousrp/Main.java
@@ -1,6 +1,7 @@
 package fr.stan1712.wetston.seriousrp;
 
-import fr.stan1712.wetston.seriousrp.commands.Cheque;
+import fr.stan1712.wetston.seriousrp.commands.Cheques;
+import fr.stan1712.wetston.seriousrp.events.Cheque;
 import fr.stan1712.wetston.seriousrp.events.Death;
 import net.milkbowl.vault.economy.Economy;
 import org.bukkit.plugin.PluginManager;
@@ -15,71 +16,74 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public final class Main extends JavaPlugin {
-	final private static Logger _log = LoggerFactory.getLogger(Main.class);
-	final public PluginManager pluginManager = getServer().getPluginManager();
+	private static final Logger _log = LoggerFactory.getLogger("SeriousRP - Main");
+	public final PluginManager pluginManager = getServer().getPluginManager();
 	public static Economy economy = null;
-	public static int spigotPluginId = 31443;
+	public static final int spigotPluginId = 31443;
 
-	public void versionCheck() {
-		final String logStep = getName() + " @ versionCheck";
+	public boolean versionCheck() {
+		final String logStep = "versionCheck";
 		final String serverVersion = getServer().getVersion();
-		_log.info("[" + logStep + "] Checking server version : " + serverVersion);
+		final String serverType = getServer().getName();
 
-		final Pattern versionPattern = Pattern.compile("[0-9][.][0-9]+", Pattern.MULTILINE);
+		_log.info("[{}] Checking server version : {} {}", logStep, serverType, serverVersion);
+
+		final Pattern versionPattern = Pattern.compile("\\d[.]\\d+", Pattern.MULTILINE);
 		final Matcher versionMatcher = versionPattern.matcher(serverVersion);
-		if(!versionMatcher.find() || (!serverVersion.contains("Spigot") && !serverVersion.contains("Paper"))) {
-			_log.error("[" + logStep + "] * Version " + serverVersion + "unknown, disabling plugin.");
+		if(!versionMatcher.find() || (!serverType.contains("Spigot") && !serverType.contains("Paper"))) {
+			_log.error("[{}] * Server type {} unknown, disabling plugin.", logStep, serverVersion);
+
 			pluginManager.disablePlugin(this);
-			return;
+			return false;
 		}
 		final String version = versionMatcher.group();
 
 		switch (version) {
-			case "1.19", "1.18", "1.17", "1.16", "1.15", "1.14", "1.13" -> {
-				_log.info("[" + logStep + "] Version check !");
-				_log.info("[" + logStep + "] If you got issues, report them on Github");
+			case "1.21", "1.20", "1.19", "1.18", "1.17", "1.16", "1.15", "1.14", "1.13" -> {
+				_log.info("[{}] Version check !", logStep);
+				_log.info("[{}] If you got issues, report them on Github", logStep);
 			}
 			case "1.12" -> {
-				_log.info("[" + logStep + "] " + version + " may have issues while running !");
-				_log.info("[" + logStep + "] If you got any, report them on Github");
-			}
-			case "1.11" -> {
-				_log.info("[" + logStep + "] " + version + " is not supported by SeriousRP.");
-				_log.info("[" + logStep + "] If you got any, report them on Github");
+				_log.info("[{}] {} may have issues while running !", logStep, version);
+				_log.info("[{}] If you got any, report them on Github", logStep);
 			}
 			default -> {
-				_log.error("[" + logStep + "] * Version is not supported by " + getName() + ", disabling plugin.");
+				_log.error("[{}] * Version {} is not supported by {}, disabling plugin.", logStep, serverVersion, getName());
+
 				pluginManager.disablePlugin(this);
+				return false;
 			}
 		}
+
+		return true;
 	}
 
 	public void dependenciesCheck() {
-		final String logStep = getName() + " @ dependenciesCheck";
-		_log.info("[" + logStep + "] Dependencies checking...");
+		final String logStep = "dependenciesCheck";
+		_log.info("[{}] Dependencies checking...", logStep);
 
-		File Tdirect = new File("plugins/Telecom");
-		if (!Tdirect.exists()) {
-			_log.warn("[" + logStep + "] {Telecom} Telecom dependency was not found ! Disabling RPMobile module.");
+		File telecomFolder = new File("plugins/Telecom");
+		if (!telecomFolder.exists()) {
+			_log.warn("[{}] {Telecom} Telecom dependency was not found ! Disabling RPMobile module.", logStep);
 			getConfig().set("Core.Modules.RPMobile", Boolean.FALSE);
 		}
 		else {
-			_log.info("[" + logStep + "] {Telecom} Vault hooked !");
+			_log.info("[{}] {Telecom} Vault hooked !", logStep);
 		}
 
 		if (!setupEconomy() ) {
-			_log.warn("[" + logStep + "] {Vault} Vault dependency was not found ! Disabling Economy modules.");
+			_log.warn("[{}] {Vault} Vault dependency was not found ! Disabling Economy modules.", logStep);
 			getConfig().set("Core.Modules.Economy", Boolean.FALSE);
 		}
 		else {
-			_log.info("[" + logStep + "] {Vault} Vault hooked !");
+			_log.info("[{}] {Vault} Vault hooked !", logStep);
 		}
 	}
 	private void updateCheck() {
-		final String logStep = getName() + " @ updateCheck";
+		final String logStep = "updateCheck";
 		new UpdateChecker(this, spigotPluginId).getVersion(version -> {
 			if(!this.getDescription().getVersion().equalsIgnoreCase(version)) {
-				_log.info("[" + logStep + "] An update is available on Spigot ! (" + version + ")");
+				_log.info("[{}] An update is available on Spigot ! ({})", logStep, version);
 			}
 		});
 	}
@@ -97,10 +101,12 @@ public final class Main extends JavaPlugin {
 	}
 
 	private void loadMetrics() {
-		final String logStep = getName() + " @ loadMetrics";
-		int _bStatsId = 7297;
-		new Metrics(this, _bStatsId);
-		_log.info("[" + logStep + "] bStats metrics loaded");
+		final String logStep = "loadMetrics";
+		final int bStatsId = 7297;
+
+		new Metrics(this, bStatsId);
+
+		_log.info("{} bStats metrics loaded", logStep);
 	}
 
 	private void loadConfig() {
@@ -108,22 +114,22 @@ public final class Main extends JavaPlugin {
 		saveConfig();
 	}
 	private void loadRecipes() {
-		final String logStep = "[" + getName() + " @ loadRecipes] ";
+		final String logStep = "[loadRecipes] ";
 		pluginManager.registerEvents(new Recipes(this), this);
-		_log.info(logStep + "All recipes have been added !");
+		_log.info("{} All recipes have been added !", logStep);
 	}
 	private void loadModules() {
-		final String logStep = "[" + getName() + " @ loadModules] ";
+		final String logStep = "[loadModules] ";
 
 		if(getConfig().getBoolean("Core.Modules.CustomRecipes")) {
-			_log.info(logStep + "CustomRecipes > ON");
+			_log.info("{} CustomRecipes > ON", logStep);
 			loadRecipes();
 		}
-		else _log.info(logStep + "CustomRecipes > OFF");
+		else _log.info("{} CustomRecipes > OFF", logStep);
 
 		pluginManager.registerEvents(new Death(this), this);
-		if(getConfig().getBoolean("Core.Modules.RPDeath")) _log.info(logStep + "RPDeath > ON");
-		else _log.info(logStep + "RPDeath > OFF");
+		if(getConfig().getBoolean("Core.Modules.RPDeath")) _log.info("{} RPDeath > ON", logStep);
+		else _log.info("{} RPDeath > OFF", logStep);
 
 		/*if(getConfig().getBoolean("Core.Modules.Medics")) {
 			getCommand("revive").setExecutor(new Revive(this));
@@ -138,8 +144,8 @@ public final class Main extends JavaPlugin {
 			_log.info(logStep + "Medics > OFF");
 		}*/
 
-		if(getConfig().getBoolean("Core.Modules.TownSystem")) _log.info(logStep + "TownSystem > ON");
-		else _log.info(logStep + "TownSystem > OFF");
+		/*if(getConfig().getBoolean("Core.Modules.TownSystem")) _log.info("{} TownSystem > ON", logStep);
+		else _log.info("{} TownSystem > OFF", logStep);*/
 
 		/*if(getConfig().getBoolean("Core.Modules.RPMobiles") == true) {
 			console.sendMessage("[SeriousRP] " + ChatColor.GREEN + "RPMobiles > ON");
@@ -147,48 +153,51 @@ public final class Main extends JavaPlugin {
 		else {
 			console.sendMessage("[SeriousRP] " + ChatColor.RED + "RPMobiles > OFF");
 		}*/
-		if(getConfig().getBoolean("Core.Modules.Chairs")) _log.info(logStep + "Chairs > ON");
-		else _log.info(logStep + "Chairs > OFF");
+		if(getConfig().getBoolean("Core.Modules.Chairs")) _log.info("{} Chairs > ON", logStep);
+		else _log.info("{} Chairs > OFF", logStep);
 
-		if(getConfig().getBoolean("Core.Modules.Economy")) _log.info(logStep + "Economy > ON");
-		else _log.info(logStep + "Economy > OFF");
+		if(getConfig().getBoolean("Core.Modules.Economy")) {
+			_log.info("{} Economy > ON", logStep);
+			pluginManager.registerEvents(new Cheque(this), this);
+		}
+		else _log.info("{} Economy > OFF", logStep);
 	}
 	private void loadCommands() {
-		final String logStep = "[" + getName() + " @ loadCommands] ";
+		final String logStep = "[loadCommands] ";
 
-		Objects.requireNonNull(getCommand("cheque")).setExecutor(new Cheque(this));
-		_log.info(logStep + "/cheque commands loaded");
+		Objects.requireNonNull(getCommand("cheque")).setExecutor(new Cheques(this));
+		_log.info("{} /cheque commands loaded", logStep);
 
-		_log.info(logStep + "All commands have been added !");
+		_log.info("{} All commands have been added !", logStep);
 	}
 
 	private void logNewStep(String step) {
-		_log.info("[" + getName() + " @-> " + step + "] ---");
+		_log.info("{@ {}} ---", step);
 	}
 
 	@Override
 	public void onEnable() {
-		//(new Thread(this::loadMetrics, getName() + " - Startup")).start();
-
 		logNewStep("updateCheck");
 		updateCheck();
 
 		logNewStep("versionCheck");
-		versionCheck();
+		final boolean versionCorrect = versionCheck();
 
-		logNewStep("dependenciesCheck");
-		dependenciesCheck();
+		if(versionCorrect) {
+			logNewStep("dependenciesCheck");
+			dependenciesCheck();
 
-		logNewStep("loadMetrics");
-		loadMetrics();
+			logNewStep("loadMetrics");
+			loadMetrics();
 
-		logNewStep("loadConfig");
-		loadConfig();
+			logNewStep("loadConfig");
+			loadConfig();
 
-		logNewStep("loadModules");
-		loadModules();
+			logNewStep("loadModules");
+			loadModules();
 
-		logNewStep("loadCommands");
-		loadCommands();
+			logNewStep("loadCommands");
+			loadCommands();
+		}
 	}
 }
